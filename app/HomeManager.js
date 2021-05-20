2e340bcc99dab17567944c296ad2295f37bfedca
Abdul Muksith Rizvi <57699415+abdulmuksith3@users.noreply.github.com>
2021-05-20 20:35:24 +0300

    Map

--- app/HomeManager.js
+++ app/HomeManager.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, Button, TouchableOpacity } from "react-native";
-import firebase from "firebase";
-import "firebase/auth";
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react'
+import { View, Text, Button, TouchableOpacity } from 'react-native'
+import firebase from 'firebase'
+import 'firebase/auth'
+import MapView from 'react-native-maps'
+import { colors } from './common/theme'
+import { Icon } from 'react-native-elements'
+import { customMapStyle} from './common/mapStyle'
+
 export default function HomeManager({ navigation }) {
   const latitudeDelta = 0.0922
   const longitudeDelta = 0.0421
@@ -10,44 +14,128 @@ export default function HomeManager({ navigation }) {
   const longitude = 51.534817
 
   return (
-
     <View style={{ flex: 1 }}>
-      <MapView style={{ flex: 1 }}
+      <MapView
+        style={{ flex: 1 }}
         showsUserLocation={true}
         provider="google"
         initialRegion={{
           latitude,
           longitude,
           latitudeDelta,
-          longitudeDelta
+          longitudeDelta,
         }}
-      // userInterfaceStyle={"dark"}
-
+        customMapStyle={customMapStyle}
+        // userInterfaceStyle={"dark"}
+      ></MapView>
+      <View
+        style={{
+          alignSelf: 'center',
+          // backgroundColor: 'red',
+          width: '90%',
+          minHeight: 60,
+          position: 'absolute',
+          top: '6%',
+        }}
       >
-      </MapView>
-      <View style={{ alignSelf:"center", backgroundColor: "red", width: "90%", minHeight: 60, position: "absolute", top: "6%" }}>
-      <TouchableOpacity style={{height:65, backgroundColor:"yellow", aspectRatio:1/1, borderRadius:100, justifyContent:"center", alignItems:"center"}}>
-            <Text>Drawer</Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            height: 55,
+            backgroundColor: colors.GREEN,
+            aspectRatio: 1 / 1,
+            borderRadius: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.84,
 
+            elevation: 5,
+          }}
+        >
+          <Icon
+                    name="menu"
+                    type="feather"
+                    color={colors.WHITE}
+                    size={25}
+                  />
+        </TouchableOpacity>
       </View>
 
-      <View style={{ alignSelf:"center", backgroundColor: "red", width: "90%", minHeight: 60, position: "absolute", bottom: "4%", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-
+      <View
+        style={{
+          alignSelf: 'center',
+          // backgroundColor: 'red',
+          width: '90%',
+          minHeight: 60,
+          position: 'absolute',
+          bottom: '4%',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
         <View>
-        <TouchableOpacity style={{height:65, backgroundColor:"yellow", aspectRatio:1/1, borderRadius:100, justifyContent:"center", alignItems:"center"}}>
-            <Text>Report List</Text>
-          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 55,
+              backgroundColor: colors.GREEN,
+              aspectRatio: 1 / 1,
+              borderRadius: 100,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
 
+              elevation: 5,
+            }}
+          >
+            <Icon
+                    name="flag"
+                    type="ionicon"
+                    color={colors.WHITE}
+                    size={28}
+                  />
+          </TouchableOpacity>
         </View>
         <View>
-        <TouchableOpacity style={{height:65, backgroundColor:"yellow", aspectRatio:1/1, borderRadius:100, justifyContent:"center", alignItems:"center"}}>
-            <Text>Chat</Text>
+          <TouchableOpacity
+            style={{
+              height: 55,
+              backgroundColor: colors.GREEN,
+              aspectRatio: 1 / 1,
+              borderRadius: 100,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowColor: '#000',
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.84,
+
+              elevation: 5,
+            }}
+          >
+            <Icon
+                    name="chatbubble-ellipses-sharp"
+                    type="ionicon"
+                    color={colors.WHITE}
+                    size={28}
+                  />
           </TouchableOpacity>
         </View>
-
       </View>
     </View>
-
-  );
+  )
 }
